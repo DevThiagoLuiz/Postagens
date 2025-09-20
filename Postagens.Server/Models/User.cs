8d76463ff8c24db95c0b87225240b8bf943f4e98
DevThiagoLuiz <thiagoluiz5123@gmail.com>
2025-09-20 13:21:19 -0300

    #Add: model and dtos

---
+++ Postagens.Server/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Postagens.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string? Bio { get; set; }
+    }
+}
